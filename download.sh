d6ef0ae4eb30c3cfb17e547c1d3e0eec7b0627f9
Wataru ENDO <endo.wataru@gmail.com>
2016-03-01 01:35:17 +0900

    Separated downloader script

---
+++ download.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -eu
+
+source config.sh
+
+cd $SOURCE_PATH
+
+# Stow
+wget -c http://ftp.gnu.org/gnu/stow/stow-$STOW_VERSION.tar.gz
+
+# Git
+wget -c https://www.kernel.org/pub/software/scm/git/git-$GIT_VERSION.tar.xz
+
+# Vim
+git clone https://github.com/vim/vim.git || true
+cd vim
+git checkout master && git pull
+
+cd $SOURCE_PATH
+
+# Python
+wget -c https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tar.xz
+
+# CMake
+# TODO: remove --no-check-certificate
+wget --no-check-certificate -c https://cmake.org/files/v$CMAKE_MAJOR_VERSION/cmake-$CMAKE_VERSION.tar.gz
+
--- install.sh
+++ install.sh
@@ -2,26 +2,11 @@
 
 set -eu
 
-ROOT_PATH=`pwd`
-SOURCE_PATH=$ROOT_PATH/src
-INSTALL_PATH=$ROOT_PATH/local/stow
-
-STOW_VERSION=2.2.2
-GIT_VERSION=2.7.0
-PYTHON_VERSION=3.5.1
-CMAKE_MAJOR_VERSION=3.4
-CMAKE_MINOR_VERSION=2
-CMAKE_VERSION=$CMAKE_MAJOR_VERSION.$CMAKE_MINOR_VERSION
-
-STOW=$ROOT_PATH/bin/stow
-GIT=$ROOT_PATH/local/bin/git
-
-mkdir $SOURCE_PATH || true
+source config.sh
 
 # Install Stow
 
 cd $SOURCE_PATH
-wget -c http://ftp.gnu.org/gnu/stow/stow-$STOW_VERSION.tar.gz
 tar xvf stow-$STOW_VERSION.tar.gz
 cd stow-$STOW_VERSION
 mkdir build || true
@@ -33,7 +18,6 @@ make install
 # Git
 
 cd $SOURCE_PATH
-wget -c https://www.kernel.org/pub/software/scm/git/git-$GIT_VERSION.tar.xz
 tar xvf git-$GIT_VERSION.tar.xz
 cd git-$GIT_VERSION
 make configure
@@ -44,12 +28,7 @@ $STOW -v git-$GIT_VERSION
 
 # Vim
 
-cd $SOURCE_PATH
-git clone https://github.com/vim/vim.git || true
-cd vim
-git checkout master
-git pull
-cd src
+cd $SOURCE_PATH/vim/src
 make distclean || true
 ./configure --prefix=$INSTALL_PATH/vim-master
 make
@@ -60,7 +39,6 @@ $STOW -v vim-master
 # Python
 
 cd $SOURCE_PATH
-wget -c https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tar.xz
 tar xvf Python-$PYTHON_VERSION.tar.xz
 cd Python-$PYTHON_VERSION
 mkdir build || true
@@ -74,7 +52,6 @@ $STOW -v python-$PYTHON_VERSION
 # CMake
 
 cd $SOURCE_PATH
-wget -c https://cmake.org/files/v$CMAKE_MAJOR_VERSION/cmake-$CMAKE_VERSION.tar.gz
 tar xvf cmake-$CMAKE_VERSION.tar.gz
 cd cmake-$CMAKE_VERSION
 ./bootstrap --prefix=$INSTALL_PATH/cmake-$CMAKE_VERSION
