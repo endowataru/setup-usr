76f7cbf25846b84a238e5471b7ab09f1c7cf8681
Wataru ENDO <endo.wataru@gmail.com>
2017-05-01 17:21:46 +0900

    Fix bug of install-nvim.sh

--- install-nvim.sh
+++ install-nvim.sh
@@ -6,7 +6,7 @@ source config.sh
 
 cd $SOURCE_PATH/neovim
 NEOVIM_HASH=`git rev-parse --short HEAD`
-rm -r build
+rm -r build || true
 make clean
 make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH/neovim-$NEOVIM_HASH"
 make install
