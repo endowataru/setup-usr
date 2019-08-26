0c695a15cfd7cae581cbfa7ad5fdc6d4b429c9d0
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2019-08-26 20:35:44 +0900

    Fix problem of installing zsh in latest version

--- install-zsh.sh
+++ install-zsh.sh
@@ -8,9 +8,8 @@ cd $SOURCE_PATH
 tar xvf zsh-$ZSH_VERSION.tar.xz
 tar xvf zsh-$ZSH_VERSION-doc.tar.xz
 cd zsh-$ZSH_VERSION
-mkdir build || true
-cd build
-../configure --prefix=$INSTALL_PATH/zsh-$ZSH_VERSION
+# TODO: use out-of-source build (need to copy Doc/)
+./configure --prefix=$INSTALL_PATH/zsh-$ZSH_VERSION
 make
 make install
 cd $INSTALL_PATH
